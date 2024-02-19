3076cb24fdc28be05095acb5c080da710d8288e3
zakaria <banidzakaria@gmail.com>
2024-02-19 11:56:09 +0100

    create classes

---
+++ src/City.java
@@ -0,0 +1,7 @@
+public abstract class City {
+    int cityId; //Identifiant unique pour la ville.
+    String cityName; //Nom de la ville.
+    int currentTemperature; //Température actuelle.
+    int currentHumidity; //Taux d'humidité actuelle.
+    int currentWindSpeed; //Vitesse du vent actuelle.
+}
---
+++ src/CityHistory.java
@@ -0,0 +1,5 @@
+public class CityHistory extends City{
+    int historicalDataId; //Identifiant unique pour les données historiques.
+    int cityId; //Identifiant de la ville associée.
+    int temperature; //Température historique.
+}
